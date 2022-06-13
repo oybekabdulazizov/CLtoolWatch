ccbe92e1a01f2141a920899028446d40b0b2684e
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-06-13 11:54:06 +0200

    add directory watcher

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const process = require('process');
 
 //#region helper function 
-const getFilename = (path) => {
+const getItemName = (path) => {
     return path.substring(path.lastIndexOf('\\') + 1);
 }
 //#endregion
@@ -16,29 +16,36 @@ programme
     .version('0.0.1')
     .argument('[filename]', 'Name of a file to execute')
     .action(() => {
-        // chokidar.watch('.').on('change', () => console.log(filename));
-        // const fname = filename || 'index.js';
 
         const add = debounce((path) => {
-            console.log(`${getFilename(path)} has been created.`);
+            console.log(`${getItemName(path)} has been created in folder ${path}`);
         }, 200);
         const change = debounce((path) => {
-            console.log(`${getFilename(path)} has been modified.`);
+            console.log(`${getItemName(path)} has been modified.`);
         }, 200);
         const unlink = debounce((path) => {
-            console.log(`${getFilename(path)} has been deleted.`);
+            console.log(`${getItemName(path)} has been deleted from foler ${path}`);
         }, 200);
-
-        // try {
-        //     await fs.promises.access(getFilename(path));
-        // } catch (err) {
-        //     throw new Error(`You either don't have proper access to the file ${fname} or it does not exist.`);
-        // }
+        const error = (error) => {
+            // the below condition is to protect from throwing an error when a folder gets deleted. 
+            // without the condition, it usually throws EPERM error, which is translated from ERROR_ACCESS_DENIED
+            if (error.errno != '-4048') {
+                console.log(error);
+            } 
+        };
 
         chokidar
             .watch(process.cwd(), { ignoreInitial: true })
             .on('add', add)
             .on('change', change)
-            .on('unlink', unlink); 
+            .on('unlink', unlink)
+            .on('error', error)
+            
+        chokidar
+            .watch(process.cwd(), { ignoreInitial: true })
+            .on('addDir', add)
+            .on('changeDir', change)
+            .on('unlinkDir', unlink)
+            .on('error', error)
     });
 programme.parse(process.argv);
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 console.log('hello from the outside!');
+console.log('i hope this app is working fine now. yes it is working');
